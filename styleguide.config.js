38e37a4387ccc7e3bc379bda720ce8b5a10cbfa6
Dmitry Sadovsky <dimailn9@gmail.com>
2021-08-30 20:53:32 +0300

    wip

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,4 +1,13 @@
 module.exports = {
-  components: 'src/components/templates/[A-z]*.vue',
-  webpackConfig: require('./build/webpack.base.conf')
+  // components: 'src/components/templates/[A-z]*.vue',
+  webpackConfig: require('./build/webpack.base.conf'),
+  sections: [
+    {
+      name: 'UI Components',
+      // content: 'docs/ui.md',
+      components: 'src/components/templates/[A-z]*.vue'
+    }
+  ],
+  usageMode: 'expand',
+  exampleMode: 'expand',
 }
