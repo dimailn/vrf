55abacf541d18644da21b773d45ea526ed13a858
Dmitry Sadovsky <dimailn9@gmail.com>
2021-09-02 21:47:19 +0300

    rf-action-result properties description

--- src/components/descriptors/action-result.js
+++ src/components/descriptors/action-result.js
@@ -1,7 +1,13 @@
 export default {
   name: 'rf-action-result',
   props: {
+    /**
+     * Name passed to rf-action
+     */
     name: String,
+    /**
+     * Component to display the result with props `data` and `status`.
+     */
     component: [String, Object]
   }
 };
