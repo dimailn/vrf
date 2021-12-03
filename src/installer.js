c07bd66bd6fccad0ce34130c2333c4d452994d95
Dmitry Sadovsky <dimailn9@gmail.com>
2021-12-03 18:57:20 +0300

    idFromRouter

--- src/installer.js
+++ src/installer.js
@@ -132,7 +132,7 @@ export default function(components) {
         return;
       }
       
-      ['translate', 'effects', 'store', 'autocompletes', 'partials', 'sources', 'dateInterceptor', 'transforms', 'locale', 'loader'].forEach((optionName) => {
+      ['translate', 'effects', 'store', 'autocompletes', 'partials', 'sources', 'dateInterceptor', 'transforms', 'locale', 'loader', 'idFromRoute'].forEach((optionName) => {
         if (options[optionName]) {
           Vue.prototype.VueResourceForm[optionName] = options[optionName]
         }
