12ef7f24941516e8eb384eed5ebda1dba17dc34f
Dmitry Sadovsky <dimailn9@gmail.com>
2023-01-26 14:19:51 +0300

    added jest-dom

--- test/unit/jest.setup.js
+++ test/unit/jest.setup.js
@@ -1,4 +1,5 @@
 import { get, def } from 'bdd-lazy-var/global'
+import '@testing-library/jest-dom'
 
 const realDescribe = describe;
 describe = function(name, fn) { realDescribe(name, () => { fn(); }); };
