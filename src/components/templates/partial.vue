10eddc0fbff6f3e9f8f2d8d40809ab2da9730b5a
Dmitry Sadovsky <dimailn9@gmail.com>
2023-01-26 16:09:57 +0300

    remove rf-partial

--- src/components/templates/partial.vue
+++
@@ -1,7 +0,0 @@
-<template>
-<component :is="component" :resource="$resource" :rf-name="$rfName" :callback="callback">
-  <slot />
-</component>
-</template>
-
-<script  src="../descriptors/partial.js" />
